4a8570789c09434f51fd8d99606200cd49e11d7e
Ryan Souza <ryancarlosamorimsouza@gmail.com>
2026-01-06 10:35:04 -0300

    atualização ex018

--- mundo1/desafios/ex018.py
+++ mundo1/desafios/ex018.py
@@ -1,13 +1,25 @@
 from math import radians, sin, cos, tan
 
-angulo = float(input('Digite um ângulo em graus: '))
+angulo = float(input('Digite o ângulo que você deseja: '))
 
 radianos = radians(angulo)
 seno = sin(radianos)
 cosseno = cos(radianos)
 tangente = tan(radianos)
 
-print(f'O angulo digitado foi: {angulo:.2f}°')
-print(f'Seno: {seno:.2f}')
-print(f'Cosseno: {cosseno:.2f}')
-print(f'Tangente: {tangente:.2f}')
+print(f'O ângulo de {angulo} tem o SENO de {seno:.2f}')
+print(f'O ângulo de {angulo} tem o COSSENO de {cosseno:.2f}')
+print(f'O ângulo de {angulo} tem a TANGENTE de {tangente:.2f}')
+
+
+"""an = float(input('Digite o ângulo que você deseja: '))
+
+seno = sin(radians(an))
+cosseno = cos(radians(an))
+tangente = tan(radians(an))
+
+print(f'O ângulo de {an} tem o SENO de {seno:.2f}')
+print(f'O ângulo de {an} tem o COSSENO de {cosseno:.2f}')
+print(f'O ângulo de {an} tem a TANGENTE de {tangente:.2f}')
+
+"""
