13f72359bbf0c3d8a1c1a3e01c85beb85585d613
Ryan Souza <ryancarlosamorimsouza@gmail.com>
2026-01-06 10:10:11 -0300

    atualização ex017 com mais uma forma de fazer

--- mundo1/desafios/ex017.py
+++ mundo1/desafios/ex017.py
@@ -1,10 +1,17 @@
-import math
+from math import hypot
 
 cateto_oposto = float(input('Comprimento do cateto oposto: '))
-
 cateto_adjacente = float(input('Comprimento do cateto adjacente: '))
 
-hipotenusa = math.hypot(cateto_oposto, cateto_adjacente)
+hipotenusa = hypot(cateto_oposto, cateto_adjacente)
 
 print(f'A hipotenusa  vai medir {hipotenusa:.2f}')
+
+
+
+'''co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = (co ** 2 + ca ** 2) ** (1/2)
+
+print(f'A hipotenusa vai medir {hi:.2f}')'''
 
