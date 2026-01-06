501805aa5dfc4f5e28eb3019717c199e09afee5e
Ryan Souza <ryancarlosamorimsouza@gmail.com>
2026-01-06 10:49:36 -0300

    Atualização ex021 + nova forma de fazer

--- mundo1/desafios/ex021.py
+++ mundo1/desafios/ex021.py
@@ -1,7 +1,15 @@
 import pygame
 
-pygame.mixer.init()
+"""pygame.mixer.init()
 pygame.mixer.music.load("/media/rc/home_nvme/ubuntu/Downloads/xxxtentacion.mp3")
 pygame.mixer.music.play()
 
-input("Enter pra parar")
+input("Enter pra parar")"""
+
+
+pygame.init()
+pygame.mixer.music.load("/media/rc/home_nvme/ubuntu/Downloads/xxxtentacion.mp3")
+pygame.mixer.music.play()
+input('Enter para parar')
+pygame.event.wait()
+
