6448fe34e94b84d7b793d5897a6df0a3f855d534
Ryan Souza <ryancarlosamorimsouza@gmail.com>
2026-01-06 10:40:22 -0300

    atualização ex019

--- mundo1/desafios/ex019.py
+++ mundo1/desafios/ex019.py
@@ -1,12 +1,13 @@
-import random
+from random import choice
 
-nome1 = input('Digite o nome do primeiro aluno: ')
-nome2 = input('Digite o nome do segundo aluno: ')
-nome3 = input('Digite o nome do terceiro aluno: ')
-nome4 = input('Digite o nome do quarto aluno: ')
+
+nome1 = input('Nome do primeiro aluno: ')
+nome2 = input('nome do segundo aluno: ')
+nome3 = input('Nome do terceiro aluno: ')
+nome4 = input('Nome do quarto aluno: ')
 
 nomes = [nome1, nome2, nome3, nome4]
 
-escolha = random.choice(nomes)
+escolha = choice(nomes)
 
-print(f'O aluno sorteado para apagar o quadro foi: "{escolha}"')
+print(f'O aluno escolhido foi: "{escolha}"')
